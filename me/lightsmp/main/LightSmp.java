493b09d202adc8d7275497fef5e98433b8a75dcd
NotEasyOk <masterplayz755@gmail.com>
2026-02-23 07:30:10 +0530

    Update LightSmp.java

--- me/lightsmp/main/LightSmp.java
+++ me/lightsmp/main/LightSmp.java
@@ -1,25 +1,31 @@
 package me.lightsmp;
 
 import me.lightsmp.listeners.*;
+import me.lightsmp.manager.CooldownManager;
+import me.lightsmp.recipes.WeaponRecipes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     private static Main instance;
+    private CooldownManager cooldownManager;
 
     @Override
     public void onEnable() {
         instance = this;
+        this.cooldownManager = new CooldownManager();
         saveDefaultConfig();
         
-        // Listeners Registration
+        // Register Everything
+        WeaponRecipes.register();
         getServer().getPluginManager().registerEvents(new CraftingSystem(), this);
         getServer().getPluginManager().registerEvents(new MaceAbility(), this);
         getServer().getPluginManager().registerEvents(new ElytraAbility(), this);
         getServer().getPluginManager().registerEvents(new SpearAbility(), this);
-        getServer().getPluginManager().registerEvents(new VanillaControl(), this);
+        getServer().getPluginManager().registerEvents(new VanillaLimiter(), this);
 
-        getLogger().info("LightSmp Active: Weapons, Recipes, and Vanilla Disabler Loaded!");
+        getLogger().info("LightSmp v1.0 - All Systems Operational!");
     }
 
     public static Main getInstance() { return instance; }
-}
+    public CooldownManager getCooldownManager() { return cooldownManager; }
+            }
